cba4e4e3710c26f2ae0fcbf6dba1c01c1af380b7
pangio <pangio.dev@gmail.com>
2015-11-01 19:08:31 -0300

    added Mapper and ServiceFactory

--- module/Blog/src/Blog/Service/PostService.php
+++ module/Blog/src/Blog/Service/PostService.php
@@ -4,58 +4,28 @@ namespace Blog\Service;
 
 class PostService implements PostServiceInterface
 {
-	protected $data = array(
-	    array(
-	       'id'    => 1,
-	       'title' => 'Hello World #1',
-	       'text'  => 'This is our first blog post!'
-	    ),
-	    array(
-	       'id'     => 2,
-	       'title' => 'Hello World #2',
-	       'text'  => 'This is our second blog post!'
-	    ),
-	    array(
-	       'id'     => 3,
-	       'title' => 'Hello World #3',
-	       'text'  => 'This is our third blog post!'
-	    ),
-	    array(
-	       'id'     => 4,
-	       'title' => 'Hello World #4',
-	       'text'  => 'This is our fourth blog post!'
-	    ),
-	    array(
-	       'id'     => 5,
-	       'title' => 'Hello World #5',
-	       'text'  => 'This is our fifth blog post!'
-	    )
-	);
+    protected $postMapper;
+
+    /**
+     * @param PostMapperInterface $postMapper
+     */
+    public function __construct(PostMapperInterface $postMapper)
+    {
+        $this->postMapper = $postMapper;
+    }
 
     /**
      * {@inheritDoc}
      */
     public function findAllPosts()
     {
-        $allPosts = array();
-        foreach ($this->data as $index => $post) {
-            $allPosts[] = $this->findPost($index);
-        }
-
-      	return $allPosts;
+        return $this->postMapper->findAll();
     }
     /**
      * {@inheritDoc}
      */
     public function findPost($id)
     {
-        $postData = $this->data[$id];
-
-        $model = new Post();
-        $model->setId($postData['id']);
-        $model->setTitle($postData['title']);
-        $model->setText($postData['text']);
-
-        return $model;
+        return $this->postMapper->find($id);
     }
 }
